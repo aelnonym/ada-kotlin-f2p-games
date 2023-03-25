1ac46599bb3b236b56921471d230f0e558b6490a
joaovictorsl <joaovictordev2003@gmail.com>
2023-03-25 02:01:07 -0300

    Adding method to fetch game detail.

--- app/src/main/java/me/joaovictorsl/adafreetoplaygames/network/FreeToGameService.kt
+++ app/src/main/java/me/joaovictorsl/adafreetoplaygames/network/FreeToGameService.kt
@@ -2,10 +2,14 @@ package me.joaovictorsl.adafreetoplaygames.network
 
 import me.joaovictorsl.adafreetoplaygames.model.Game
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface FreeToGameService {
 
     @GET("games")
     suspend fun getAllGames(): List<Game>
+
+    @GET("game")
+    suspend fun getGameById(@Query("id") id: Int): Game
 
 }
